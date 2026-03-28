34251f8c496ad62bce6f90c6efafe27b3a8f0bc1
xcodedave <xcodedave@gmail.com>
2026-03-28 13:36:55 +0000

    Add Flight demo

---
+++ experiments/flight/pkg/flight_web.d.ts
@@ -0,0 +1,45 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function main(): Promise<void>;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly main: () => void;
+    readonly wasm_bindgen__closure__destroy__h085bc2834b5014d3: (a: number, b: number) => void;
+    readonly wasm_bindgen__closure__destroy__h02a7efe590e0ade6: (a: number, b: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h29129068225bac13: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__hd9e22471006791b0: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h32c3250b5b15a569: (a: number, b: number) => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ experiments/flight/pkg/flight_web_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: () => void;
+export const wasm_bindgen__closure__destroy__h085bc2834b5014d3: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__h02a7efe590e0ade6: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h29129068225bac13: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__hd9e22471006791b0: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h32c3250b5b15a569: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
